2f7b8b452aa5b31cc4efe18f6bfe291a693d5ec9
KYOHEI-jp <kingdomhearts510@gmail.com>
2022-03-01 21:52:36 +0900

    中身修正

---
+++ lib/screens/homepage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatefulWidget {
+  const Homepage({Key? key}) : super(key: key);
+
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
