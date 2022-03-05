3ae87d74c25bc82e71942fc6f9a524ca3c447ffa
KYOHEI-jp <kingdomhearts510@gmail.com>
2022-03-05 18:41:14 +0900

    user_input改良

--- lib/widgets/user_input.dart
+++ lib/widgets/user_input.dart
@@ -7,6 +7,27 @@ class UserInput extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 8),
+      child: Row(
+        children: [
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 5),
+              child: TextField(),
+            ),
+          ),
+          const SizedBox(
+            width: 10,
+          ),
+          GestureDetector(
+            onTap: () {},
+            child: Container(
+              color: Colors.red,
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 8),
+              child: const Text("Add"),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
